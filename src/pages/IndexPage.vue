dd0290d56265d4115eb8b5d05da0f8bdbaca42eb
prog <email@gmail.com>
2023-11-16 22:51:22 +0700

    IndexPage

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,17 @@
+<!-- src/pages/Index.vue -->
+<template>
+  <q-page class="flex flex-center">
+    <movie-list />
+  </q-page>
+</template>
+
+<script>
+import MovieList from 'components/MovieList.vue';
+
+export default {
+  name: 'PageIndex',
+  components: {
+    MovieList,
+  },
+};
+</script>
